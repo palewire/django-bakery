9641d70671a90e0b5bf5c5c3fbbda83dc3fd324c
palewire <ben.welsh@gmail.com>
2012-02-28 16:22:06 -0800

    Cleaned up build a little bit with get_callable trick

--- bakery/management/commands/build.py
+++ bakery/management/commands/build.py
@@ -1,12 +1,12 @@
 import os
 import shutil
 from django.conf import settings
-from optparse import make_option
 from django.core import management
 from django.shortcuts import render
 from django.test.client import RequestFactory
+from django.core.urlresolvers import get_callable
 from django.core.management.base import BaseCommand, CommandError
-from django.core.urlresolvers import reverse
+
 
 class Command(BaseCommand):
     help = 'Bake out the entire site as flat files in the build directory'
@@ -17,10 +17,14 @@ class Command(BaseCommand):
         outfile.close()
     
     def handle(self, *args, **options):
+        """
+        Making it happen.
+        """
         # Destroy the build directory, if it exists
         self.stdout.write("Creating build directory\n")
         if os.path.exists(settings.BUILD_DIR):
             shutil.rmtree(settings.BUILD_DIR)
+        
         # Then recreate it from scratch
         os.makedirs(settings.BUILD_DIR)
         
@@ -36,14 +40,16 @@ class Command(BaseCommand):
             media_path
         )
         shutil.copytree(settings.STATIC_ROOT, static_path)
-
+        
+        # Build views
         try:
             settings.BAKERY_VIEWS
         except AttributeError:
-            raise AttributeError("No views in settings.BAKERY_VIEWS")
-
+            raise CommandError("No views in settings.BAKERY_VIEWS")
+        
         for view_str in settings.BAKERY_VIEWS:
-            view = self._view_from_string(view_str)
+            self.stdout.write("Building %s\n" % view_str)
+            view = get_callable(view_str)
             view().build_queryset()
         
         # Build 404 page
@@ -52,15 +58,3 @@ class Command(BaseCommand):
         response = render(rf.get("/404.html"), '404.html', {})
         self.write('404.html', response.content)
 
-    def _view_from_string(self, name):
-        """
-        Takes a full string representing a dot-path to 
-        a class (eg "blog.views.DetailView") and returns that
-        class as a Python object.
-        """
-        mod = __import__(name.split('.')[0])
-        bits = name.split('.')[1:]
-        c = mod
-        for bit in bits:
-            c = getattr(c, bit)
-        return c
