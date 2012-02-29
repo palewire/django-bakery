57d78aac6d83f99420d2d2e0bcd258b7882cdd45
palewire <ben.welsh@gmail.com>
2012-02-28 16:34:15 -0800

    Added a buildable list view

--- bakery/views.py
+++ bakery/views.py
@@ -3,7 +3,6 @@ Views that inherit from Django's class-based generic views
 and add methods that make it convenient for building out 
 flat files.
 """
-
 import os
 import logging
 from django.conf import settings
@@ -11,6 +10,7 @@ from django.test.client import RequestFactory
 from django.views.generic import TemplateView, DetailView
 
 logger = logging.getLogger(__name__)
+
 
 class BuildableTemplateView(TemplateView):
 
@@ -29,6 +29,32 @@ class BuildableTemplateView(TemplateView):
         os.path.exists(path) or os.makedirs(path)
         path = os.path.join(path, 'index.html')
         # Write out the data
+        outfile = open(path, 'w')
+        outfile.write(html)
+        outfile.close()
+
+
+class BuildableListView(ListView):
+    """
+    A list of all tables.
+    """
+    build_path = 'index.html'
+    
+    def build_queryset(self):
+        """
+        Build the view as a flat HTML file.
+        
+        Example usage:
+            
+            TableListView().build_queryset()
+        
+        """
+        # Make a fake request
+        self.request = RequestFactory().get(self.build_path)
+        # Render the list page as HTML
+        html = self.get(self.request).render().content
+        # Write it out to the appointed flat file
+        path = os.path.join(settings.BUILD_DIR, self.build_path)
         outfile = open(path, 'w')
         outfile.write(html)
         outfile.close()
