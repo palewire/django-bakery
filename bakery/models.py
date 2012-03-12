8ee19726eac720f8a3ee402a2e17f14f225afe67
palewire <ben.welsh@gmail.com>
2012-03-12 16:07:52 -0700

    Docstring

--- bakery/models.py
+++ bakery/models.py
@@ -1,11 +1,12 @@
 """
-Models that inherit from Django's and add methods that 
-make it convenient for building out flat files. The building
-magic here really relies on your view being class-based
-and having a build_object method, like the BuildableDetailView 
-included in this app.
+Models that inherit from Django's and add methods that make it convenient for 
+building out flat files.
+
+The magic relies on your view being class-based and having a build_object
+method, like the BuildableDetailView included in this app.
 """
 from django.db import models
+
 
 class BuildableModel(models.Model):
     """
@@ -31,17 +32,15 @@ class BuildableModel(models.Model):
     
     def _build_extra(self):
         """
-        Build extra content, like copying an image to
-        a thumbnails folder under the media folder.
+        Build extra content, like copying an image to a thumbnails folder under
+        the media folder.
         """
         pass
     
     def build(self):
         """
-        Iterates through the views pointed to by 
-        self.detail_views, runs build_object 
-        with `self`, and calls_build_extra() and 
-        _build_related()
+        Iterates through the views pointed to by self.detail_views, runs
+        build_object with `self`, and calls_build_extra() and _build_related().
         """
         for detail_view in self.detail_views:
             view = self._get_view(detail_view)
