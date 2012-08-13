a88c6b200f67419f3f529976798f1f33516c8fea
palewire <ben.welsh@gmail.com>
2012-08-13 14:44:48 -0700

    Build and publish commands refactored to allow for more override options. Fixes #9.

--- bakery/management/commands/build.py
+++ bakery/management/commands/build.py
@@ -1,62 +1,107 @@
 import os
 import shutil
 from django.conf import settings
+from optparse import make_option
 from django.core import management
-from django.shortcuts import render
-from django.test.client import RequestFactory
 from django.core.urlresolvers import get_callable
+from django.core.exceptions import ViewDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
 
 
+custom_options = (
+    make_option(
+        "--build-dir",
+        action="store",
+        dest="build_dir",
+        default='',
+        help="Specify the path of the build directory. Will use settings.BUILD_DIR by default."
+    ),
+    make_option(
+        "--skip-static",
+        action="store_true",
+        dest="skip_static",
+        default=False,
+        help="Skip collecting the static files when building."
+    ),
+    make_option(
+        "--skip-media",
+        action="store_true",
+        dest="skip_media",
+        default=False,
+        help="Skip collecting the media files when building."
+    ),
+)
+
+
 class Command(BaseCommand):
-    help = 'Bake out the entire site as flat files in the build directory'
-    
-    def write(self, path, data):
-        outfile = open(os.path.join(settings.BUILD_DIR, path), 'w')
-        outfile.write(data)
-        outfile.close()
+    help = 'Bake out a site as flat files in the build directory'
+    option_list = BaseCommand.option_list + custom_options
+    build_unconfig_msg = "Build directory unconfigured. Set BUILD_DIR in settings.py or provide it with --build-dir"
+    views_unconfig_msg = "Bakery views unconfigured. Set BAKERY_VIEWS in settings.py or provide a list as arguments."
     
     def handle(self, *args, **options):
         """
         Making it happen.
         """
+        self.verbosity = int(options.get('verbosity'))
+        
+        # Figure out what build directory to use
+        if options.get("build_dir"):
+            self.build_dir = options.get("build_dir")
+            settings.BUILD_DIR = self.build_dir
+        else:
+            if not hasattr(settings, 'BUILD_DIR'):
+                raise CommandError(self.build_unconfig_msg)
+            self.build_dir = settings.BUILD_DIR
+        
         # Destroy the build directory, if it exists
-        self.stdout.write("Creating build directory\n")
-        if os.path.exists(settings.BUILD_DIR):
-            shutil.rmtree(settings.BUILD_DIR)
+        if self.verbosity > 1:
+            print "Creating build directory"
+        if os.path.exists(self.build_dir):
+            shutil.rmtree(self.build_dir)
         
         # Then recreate it from scratch
-        os.makedirs(settings.BUILD_DIR)
+        os.makedirs(self.build_dir)
         
         # Build up static files
-        management.call_command("collectstatic", interactive=False, verbosity=0)
-        
-        # Copy the media directory
-        self.stdout.write("Building static media\n")
-        media_path = os.path.join(settings.BUILD_DIR, 'media')
-        static_path = os.path.join(settings.BUILD_DIR, 'static')
-        if os.path.exists(settings.MEDIA_ROOT):
-            shutil.copytree(
-                settings.MEDIA_ROOT,
-                media_path
+        if not options.get("skip_static"):
+            if self.verbosity > 1:
+                print "Creating static directory"
+            management.call_command(
+                "collectstatic",
+                interactive=False,
+                verbosity=0
             )
-        if os.path.exists(settings.STATIC_ROOT):
-            shutil.copytree(settings.STATIC_ROOT, static_path)
+            if os.path.exists(settings.STATIC_ROOT) and settings.STATIC_URL:
+                shutil.copytree(
+                    settings.STATIC_ROOT,
+                    os.path.join(self.build_dir, settings.STATIC_URL[1:])
+                )
         
-        # Build views
-        try:
-            settings.BAKERY_VIEWS
-        except AttributeError:
-            raise CommandError("No views in settings.BAKERY_VIEWS")
+        # Build the media directory
+        if not options.get("skip_media"):
+            if self.verbosity > 1:
+                print "Building media directory"
+            if os.path.exists(settings.MEDIA_ROOT) and settings.MEDIA_URL:
+                shutil.copytree(
+                    settings.MEDIA_ROOT,
+                    os.path.join(self.build_dir, settings.MEDIA_URL[1:])
+                )
         
-        for view_str in settings.BAKERY_VIEWS:
-            self.stdout.write("Building %s\n" % view_str)
-            view = get_callable(view_str)
-            view().build_method()
+        # Figure out what views we'll be using
+        if args:
+            view_list = args
+        else:
+            if not hasattr(settings, 'BAKERY_VIEWS'):
+                raise CommandError(self.views_unconfig_msg)
+            view_list = settings.BAKERY_VIEWS
         
-        # Build 404 page
-        self.stdout.write("Building 404 page\n")
-        rf = RequestFactory()
-        response = render(rf.get("/404.html"), '404.html', {})
-        self.write('404.html', response.content)
-
+        # Then loop through and run them all
+        for view_str in view_list:
+            if self.verbosity > 1:
+                print "Building %s" % view_str
+            try:
+                view = get_callable(view_str)
+                view().build_method()
+            except (TypeError, ViewDoesNotExist):
+                raise CommandError("View %s does not work." % view_str)
--- bakery/management/commands/publish.py
+++ bakery/management/commands/publish.py
@@ -1,15 +1,89 @@
 import os
 import subprocess
 from django.conf import settings
+from optparse import make_option
 from django.core.management.base import BaseCommand, CommandError
 
 
+custom_options = (
+    make_option(
+        "--config",
+        action="store",
+        dest="config",
+        default='',
+        help="Specify the path of an s3cmd configuration file. Will use ~/.s3cmd by default."
+    ),
+    make_option(
+        "--build-dir",
+        action="store",
+        dest="build_dir",
+        default='',
+        help="Specify the path of the build directory. Will use settings.BUILD_DIR by default."
+    ),
+    make_option(
+        "--aws-bucket-name",
+        action="store",
+        dest="aws_bucket_name",
+        default='',
+        help="Specify the AWS bucket to synce with. Will use settings.AWS_BUCKET_NAME by default."
+    ),
+)
+
+
 class Command(BaseCommand):
-    help = "Syncs the build directory with the Amazon S3 bucket defined in settings.py"
+    help = "Syncs the build directory with Amazon S3 bucket using s3cmd"
+    option_list = BaseCommand.option_list + custom_options
+    build_missing_msg = "Build directory does not exist. Cannot publish something before you build it."
+    build_unconfig_msg = "Build directory unconfigured. Set BUILD_DIR in settings.py or provide it with --build-dir"
+    bucket_unconfig_msg = "AWS bucket name unconfigured. Set AWS_BUCKET_NAME in settings.py or provide it with --aws-bucket-name"
     
-    def handle(self, *args, **kwds):
-        if not os.path.exists(settings.BUILD_DIR):
-            raise CommandError("Build directory does not exist. Cannot publish something before you build it.")
-        cmd = "s3cmd sync --delete-removed --acl-public %s/ s3://%s"
-        subprocess.call(cmd % (settings.BUILD_DIR, settings.AWS_BUCKET_NAME),
-            shell=True)
+    def handle(self, *args, **options):
+        """
+        Cobble together an s3cmd command with all the proper options and run it.
+        """
+        # The s3cmd basic command, before we append all the options.
+        cmd = "s3cmd sync --delete-removed --acl-public"
+        
+        # If the user specifies a build directory...
+        if options.get('build_dir'):
+            # ... validate that it is good.
+            if not os.path.exists(options.get('build_dir')):
+                raise CommandError(self.build_missing_msg)
+            # Go ahead of use it
+            self.build_dir = options.get("build_dir")
+        # If the user does not specify a build dir...
+        else:
+            # Check if it is set in settings.py
+            if not hasattr(settings, 'BUILD_DIR'):
+                raise CommandError(self.build_unconfig_msg)
+            # Then make sure it actually exists
+            if not os.path.exists(settings.BUILD_DIR):
+                raise CommandError(self.build_missing_msg)
+            # Go ahead of use it
+            self.build_dir = settings.BUILD_DIR
+        
+        # Append the build dir to our basic s3cmd command
+        cmd += " %s/" % self.build_dir
+        
+        # If the user has specified a custom config path append that
+        if options.get('config'):
+            cmd += ' --config=%(config)s' % options
+        
+        # If the user provides a bucket name, use that.
+        if options.get("aws_bucket_name"):
+            self.aws_bucket_name = options.get("aws_bucket_name")
+        else:
+            # Otherwise try to find it the settings
+            if not hasattr(settings, 'AWS_BUCKET_NAME'):
+                raise CommandError(self.bucket_unconfig_msg)
+            self.aws_bucket_name = settings.AWS_BUCKET_NAME
+        
+        # Append the AWS bucket name to the command
+        cmd += ' s3://%s' % self.aws_bucket_name
+        
+        # Print out the command unless verbosity is above the default
+        if int(options.get('verbosity')) > 1:
+            print 'Executing %s' % cmd
+        
+        # Execute the command
+        subprocess.call(cmd, shell=True)
