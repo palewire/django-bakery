c5b5bcb4ac749397dd02643967556efbec579b10
emamd <armand.emamdjomeh@latimes.com>
2014-05-29 13:09:51 -0700

    self.build_dir

--- bakery/management/commands/publish.py
+++ bakery/management/commands/publish.py
@@ -77,7 +77,7 @@ in settings.py or provide it with --aws-bucket-name"
                 continue # don't try to upload directories
 
             # get the relative path to the file, which is also the s3 key name
-            file_key = os.path.join(os.path.relpath(dirname, build_dir), fname)
+            file_key = os.path.join(os.path.relpath(dirname, self.build_dir), fname)
 
             # test if the filename matches the gzip pattern
             gzip_match = re.search(gzip_file_match, filename)
