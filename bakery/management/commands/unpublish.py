49e5c3148283fdf3ed2ac5195e22c8833add8789
palewire <ben.welsh@gmail.com>
2016-01-21 04:59:56 -0800

    A little more logging for #72

--- bakery/management/commands/unpublish.py
+++ bakery/management/commands/unpublish.py
@@ -1,19 +1,28 @@
 import boto
+import logging
 from django.conf import settings
 from django.core.management.base import BaseCommand
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
     help = "Empties the Amazon S3 bucket defined in settings.py"
 
-    def handle(self, *args, **kwds):
-        conn = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
-                               settings.AWS_SECRET_ACCESS_KEY)
+    def handle(self, *args, **options):
+        # Pull all the keys from the bucket
+        conn = boto.connect_s3(
+            settings.AWS_ACCESS_KEY_ID,
+            settings.AWS_SECRET_ACCESS_KEY
+        )
         self.bucket = conn.get_bucket(settings.AWS_BUCKET_NAME)
         self.keys = list(key.name for key in self.bucket.list())
+
         # Break list of keys to delete into list of lists, each with 100 keys
         self.key_chunks = []
         for i in range(0, len(self.keys), 100):
             self.key_chunks.append(self.keys[i:i+100])
         for chunk in self.key_chunks:
             self.bucket.delete_keys(chunk)
+
+        # A little logging
+        logger.info("unpublish completed, %d deleted files" % (len(self.keys))
