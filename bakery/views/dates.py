c655939b28eaec3cdf796d93b23ff7610988b1fb
palewire <ben.welsh@gmail.com>
2016-03-19 09:31:29 -0700

    Pretty sure Buildable ArchiveIndexView works now

--- bakery/views/dates.py
+++ bakery/views/dates.py
@@ -11,11 +11,22 @@ class BuildableArchiveIndexView(ArchiveIndexView, BuildableMixin):
     """
     Renders and builds a top-level archive of date-based items.
 
+    Required attributes:
+
+        model or queryset:
+            Where the list of objects should come from. `self.queryset` can
+            be any iterable of items, not just a queryset.
+
         build_path:
             The target location of the built file in the BUILD_DIR.
             `index.html` would place it at the built site's root.
             `archive/index.html` would place it inside a subdirectory.
             `archive/index.html is the default.
+
+        template_name:
+            The name of the template you would like Django to render. You need
+            to override this if you don't want to rely on the Django defaults.
+
     """
     build_path = 'archive/index.html'
 
--- example/date_views/views.py
+++ example/date_views/views.py
@@ -6,3 +6,5 @@ from bakery.views import BuildableArchiveIndexView
 class MyIndexView(BuildableArchiveIndexView):
     model = Article
     date_field = 'pub_date'
+    #allow_future = True
+    #paginate_by = 2
