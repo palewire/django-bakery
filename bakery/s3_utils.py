4c5049d8ab61c16fb222b9640f7bb714c3b9fa3f
Peter Sheats <sheats@gmail.com>
2017-02-28 10:40:25 -0500

    Test coverage

--- bakery/s3_utils.py
+++ bakery/s3_utils.py
@@ -7,31 +7,28 @@ s3 = boto3.resource('s3')
 
 
 def get_s3_client():
-    access_key = ''
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID', '')
     if hasattr(settings, 'AWS_ACCESS_KEY_ID'):
         access_key = settings.AWS_ACCESS_KEY_ID
-    access_key = os.environ.get('AWS_ACCESS_KEY_ID', access_key)
 
-    secret_key = ''
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY', '')
     if hasattr(settings, 'AWS_SECRET_ACCESS_KEY'):
         secret_key = settings.AWS_SECRET_ACCESS_KEY
-    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY', secret_key)
 
-    return boto3.client(
-        's3',
+    session = boto3.setup_default_session(
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_key)
+    return boto3.client('s3')
 
 
-def get_all_objects_in_bucket(aws_bucket_name, s3_client=None):
+def get_all_objects_in_bucket(aws_bucket_name, s3_client=None, max_keys=1000):
     if not s3_client:
         s3_client = get_s3_client()
 
     obj_dict = {}
     continuation_token = ''
-
     while True:
-        kwargs = {'Bucket': aws_bucket_name}
+        kwargs = {'Bucket': aws_bucket_name, 'MaxKeys': max_keys}
         if continuation_token:
             kwargs['ContinuationToken'] = continuation_token
         list_objects_response = s3_client.list_objects_v2(**kwargs)
