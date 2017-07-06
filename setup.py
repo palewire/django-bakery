d710f3ed9c79cb1d09280cfe6b269d09e1ded65f
palewire <ben.welsh@gmail.com>
2017-07-06 12:32:17 -0700

    v0.10.4

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class TestCommand(Command):
 
 setup(
     name='django-bakery',
-    version='0.10.3',
+    version='0.10.4',
     description='A set of helpers for baking your Django site out as flat files',
     author='The Los Angeles Times Data Desk',
     author_email='datadesk@latimes.com',
