cd4a7f89eb8195564d11f8ededa507a6940887a3
palewire <ben.welsh@gmail.com>
2017-12-24 13:07:19 -0800

    A little logging

--- bakery/apps.py
+++ bakery/apps.py
@@ -1,6 +1,8 @@
 import fs
+import logging
 from django.conf import settings
 from django.apps import AppConfig
+logger = logging.getLogger(__name__)
 
 
 class BakeryConfig(AppConfig):
@@ -8,4 +10,6 @@ class BakeryConfig(AppConfig):
     verbose_name = "Bakery"
 
     def ready(self):
-        self.filesystem = fs.open_fs(getattr(settings, 'BAKERY_FILESYSTEM', "osfs:///"))
+        self.filesystem_name = getattr(settings, 'BAKERY_FILESYSTEM', "osfs:///")
+        logger.debug("Loading filesystem at {}".format(self.filesystem_name))
+        self.filesystem = fs.open_fs(self.filesystem_name)
