e42f367cb49e1e79564e85b59fff8fd22d5d3085
temaput <putilkin@gmail.com>
2018-01-12 18:00:57 +0300

    Update apps.py
    putting fs settings to ready breaks the app registry
    it breaks wagtail_bakery

--- bakery/apps.py
+++ bakery/apps.py
@@ -8,8 +8,9 @@ logger = logging.getLogger(__name__)
 class BakeryConfig(AppConfig):
     name = 'bakery'
     verbose_name = "Bakery"
-
+    filesystem_name = getattr(settings, 'BAKERY_FILESYSTEM', "osfs:///")
+    logger.debug("Loading filesystem at {}".format(filesystem_name))
+    filesystem = fs.open_fs(self.filesystem_name)
+    
     def ready(self):
-        self.filesystem_name = getattr(settings, 'BAKERY_FILESYSTEM', "osfs:///")
-        logger.debug("Loading filesystem at {}".format(self.filesystem_name))
-        self.filesystem = fs.open_fs(self.filesystem_name)
+        pass
