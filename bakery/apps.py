45dcdc63f2f347d5bb7fb459496188c20d3f2f55
palewire <ben.welsh@gmail.com>
2018-01-16 15:48:50 +0200

    A little mopup

--- bakery/apps.py
+++ bakery/apps.py
@@ -9,8 +9,4 @@ class BakeryConfig(AppConfig):
     name = 'bakery'
     verbose_name = "Bakery"
     filesystem_name = getattr(settings, 'BAKERY_FILESYSTEM', "osfs:///")
-    logger.debug("Loading filesystem at {}".format(filesystem_name))
     filesystem = fs.open_fs(filesystem_name)
-
-    def ready(self):
-        pass
